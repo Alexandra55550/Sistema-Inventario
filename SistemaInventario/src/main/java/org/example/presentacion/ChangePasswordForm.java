6a2f2e236810d91912edc950159b4b43e0a611e4
Sonia Leon <Esmeralda500inge@outlook.com>
2025-06-08 10:45:19 -0600

    hice dos formularios

--- SistemaInventario/src/main/java/org/example/presentacion/ChangePasswordForm.java
+++ SistemaInventario/src/main/java/org/example/presentacion/ChangePasswordForm.java
@@ -3,4 +3,8 @@ package org.example.presentacion;
 import javax.swing.*;
 
 public class ChangePasswordForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField txtEmail;
+    private JPasswordField txtpassword;
+    private JButton btnchangePassword;
 }
--- SistemaInventario/src/main/java/org/example/presentacion/LoginForm.java
+++ SistemaInventario/src/main/java/org/example/presentacion/LoginForm.java
@@ -3,4 +3,9 @@ package org.example.presentacion;
 import javax.swing.*;
 
 public class LoginForm  extends JDialog {
+    private JPanel mainPanel;
+    private JTextField txtEmail;
+    private JPasswordField txtpassword;
+    private JButton btnLogin;
+    private JButton btnSalir;
 }
